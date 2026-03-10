7991655d8bd194df6f70defc081d16668716a939
amen0027 <amen0027@student.monash.edu>
2026-03-10 21:14:08 +1100

    Initial Commit

---
+++ ProjectApex/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProjectApex
+//
+//  Created by Arnav Menon on 10/3/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProjectApex/ProjectApexApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectApexApp.swift
+//  ProjectApex
+//
+//  Created by Arnav Menon on 10/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct ProjectApexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
